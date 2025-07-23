dcc70a5ed0a6d8e4a19eab5652d98afab61aad7d
elyas5203 <elyas.bahmanabadi@gmail.com>
2025-07-23 18:17:21 +0330

    remove all

--- migrations/002_create_dynamic_form_record.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO `forms` (`id`, `form_name`, `form_description`, `created_by`) VALUES
-(2, 'فرم خوداظهاری پویا', 'این فرم توسط ادمین برای خوداظهاری مدرسان طراحی می‌شود.', 1);
--- migrations/004_create_dynamic_forms_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE `dynamic_forms` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `form_name` varchar(255) NOT NULL,
-  `form_structure` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin NOT NULL CHECK (json_valid(`form_structure`)),
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
